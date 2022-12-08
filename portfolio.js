c72b9387d5f8dd6181365d1ae3dc0ae8281c8e8a
Emanuele Nepa <nepaemanuele@outlook.it>
2022-12-08 14:56:41 +0100

    -Add pictures of the projects; -Sort all the pcitures in specific folders; -Add ternary operator instead of "if-else" statement to clean code.

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,19 @@
 // Rumbling 20's Remake 
 var i = 1;
-var rumbling20RemakeImgs = ["/Photos/rumbling_20_s_remake.png","/Photos/facebook.png","/Photos/github.png","Photos/dirty_city.PNG"];
+var rumbling20RemakeImgs = ["/Photos/Rumbling20'sRemake/rumbling_20_s_remake_0.png","/Photos/Rumbling20'sRemake/rumbling_20_s_remake_1.jpg",
+                            "/Photos/Rumbling20'sRemake/rumbling_20_s_remake_2.jpg","/Photos/Rumbling20'sRemake/rumbling_20_s_remake_3.jpg",
+                            "/Photos/Rumbling20'sRemake/rumbling_20_s_remake_4.jpg"];
 // Nightmare Before Eco Congress
 var j = 1;
-var nigthmareBeforeEcoCongressImgs = ["/Photos/nightmare_before_eco_congress.png","/Photos/facebook.png","/Photos/github.png","Photos/dirty_city.PNG"];
+var nigthmareBeforeEcoCongressImgs = ["/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_0.png","/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_1.jpg",
+                                      "/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_2.jpg","/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_3.jpg",
+                                      "/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_4.jpg","/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_5.jpg",
+                                      "/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_6.jpg","/Photos/NightmareBeforeEcoCongress/nightmare_before_eco_congress_7.jpg"];
 // Dirty City
 var k = 1;
-var dirtyCityImgs = ["/Photos/dirty_city.PNG","/Photos/blank-head-profile-pic-for-a-man.jpg","/Photos/github.png","Photos/dirty_city.PNG"];
+var dirtyCityImgs = ["Photos/DirtyCity/dirty_city_0.jpg","Photos/DirtyCity/dirty_city_1.jpg",
+                     "Photos/DirtyCity/dirty_city_2.jpg","Photos/DirtyCity/dirty_city_3.jpg",
+                     "Photos/DirtyCity/dirty_city_4.jpg","Photos/DirtyCity/dirty_city_5.PNG"];
 
 function nextImage(id)
 {
@@ -15,41 +22,18 @@ function nextImage(id)
     switch(element.name)
     {
         case "Rumbling 20's Remake":
-            changeImage(element,rumbling20RemakeImgs,i); 
-            if(i >= rumbling20RemakeImgs.length - 1)   // check if last image is shown
-            {
-                i = 0; // first image
-            } 
-            else
-            {
-                i++; // next image
-            }
+            changeImage(element,rumbling20RemakeImgs,i);
+            i>= rumbling20RemakeImgs.length - 1 ? i = 0 : i++; // if we reached last image, we restart the count
             break;
         
         case "Nightmare Before Eco Congress":
             changeImage(element,nigthmareBeforeEcoCongressImgs,j);
-            if(j >= nigthmareBeforeEcoCongressImgs.length - 1) // check if last image is shown
-            {
-                j = 0; // first image
-            } 
-            else
-            {
-                j++; // next image
-            }
+            j>= nigthmareBeforeEcoCongressImgs.length - 1 ? j = 0 : j++; // if we reached last image, we restart the count
             break;
         
         case "Dirty City":
             changeImage(element,dirtyCityImgs,k);
-            if(k >= dirtyCityImgs.length - 1) // check if last image is shown
-            {
-                k = 0; // first image
-            } 
-            else
-            {
-                k++; // next image
-            }
-            break;
-        
+            k>= dirtyCityImgs.length - 1 ? k = 0 : k++; // if we reached last image, we restart the count
         default:
             break;
     }
